7fa8ee7128917aa274434fd53aea1978954a0d83
zqj-Buff <zhaoquanjun0@qq.com>
2020-05-12 10:35:47 +0800

    animate mouse event

--- src/animations/animate.js
+++ src/animations/animate.js
@@ -6,14 +6,17 @@ const animate = (function ($) {
         this.version = '0.01'
         this.params = Object.assign(
           {
+            id: '',
             color: 'skyblue',
-            direction: 'left'
+            direction: 'left',
+            controlName: 'button',
+            eventName: 'scale'
           },
           options ? options : {}
         )
         this.control = this.params.id ? $('#' + this.params.id) : null
-        this.needsJs = ['', ''];
-        this.isMoreDeal = ['', '']
+        this.needsJs = ['Mousemove', '']
+        this.isMoreDeal = ['Mousemove', '']
       }
 
       _init() {
@@ -21,27 +24,50 @@ const animate = (function ($) {
       }
 
       _doBindEvent() {
-        if (this.needsJs.indexOf(this.params.eventName)) {
+        if (this.needsJs.indexOf(this.params.eventName) > -1) {
           this._addEvent()
         }
       }
 
       _addEvent() {
         this.control
-          .on('mouseenter', () => {
-            console.log('enter')
+          .on('mouseenter', (e) => {
+            e.preventDefault()
+            e.stopPropagation()
             this.control.addClass('ani-' + this.params.eventName)
           })
-          .on('mousemove', () => {
-            console.log('move')
-            if (this.isMoreDeal.indexOf(this.params.eventName)) {
-              [this.params.controlName + this.params.eventName]()
+          .on('mousemove', (e) => {
+            e.preventDefault()
+            e.stopPropagation()
+            if (this.isMoreDeal.indexOf(this.params.eventName) > -1) {
+              ;this[this.params.controlName + this.params.eventName].call(this.control, e)
             }
           })
-          .on('mouseleave', () => {
-            console.log('leave')
+          .on('mouseleave', (e) => {
+            e.preventDefault()
+            e.stopPropagation()
             this.control.removeClass('ani-' + this.params.eventName)
+            if (this[this.params.controlName + 'mouseleave']) {
+              this[this.params.controlName + 'mouseleave'].call(this.control)
+            }
           })
+      }
+
+      imageMousemove(e) {
+        const selfW = $(this).width()
+        const selfH = $(this).height()
+        const offset = $(e.target).offset()
+        const disX = e.pageX - offset.left - selfW / 2
+        const disY = e.pageY - offset.top - selfH / 2
+        const rotateX = (disY / (selfH / 2)) * 10 + 'deg'
+        const rotateY = (-disX / (selfW / 2)) * 10 + 'deg'
+        $(this).css({
+          transform: `perspective(300px) rotateX(${rotateX}) rotateY(${rotateY}) scale3d(1, 1, 1)`
+        })
+      }
+
+      imageMouseleave() {
+        $(this).css({transform: `rotateX(0deg) rotateY(0deg)`})
       }
     }
 
